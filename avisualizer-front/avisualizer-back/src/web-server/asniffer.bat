5088773acf01fba968cbe9fc69b4dcb62624079f
everaldo <everaldogjr@gmail.com>
2021-06-22 09:08:30 -0300

    fixed batch file for windows app

--- avisualizer-front/avisualizer-back/src/web-server/asniffer.bat
+++ avisualizer-front/avisualizer-back/src/web-server/asniffer.bat
@@ -1,15 +1,16 @@
 @echo off
 mkdir %4\projects\%2
 cd %4\asniffer
-jar -xvf .\%1
-cd ..\
-java -jar %4\asniffer\asniffer.jar -p %4\asniffer\%2 -r %4\projects\ -t jsonAV
+jar -xvf %3
+java -jar asniffer.jar -p .\%2 -r %4\projects\ -t jsonAV
 :loop
     if not exist %4"\projects\asniffer_results" (
         ping -n 2 localhost > nul
         goto loop
     )
+del %4\asniffer\%3
 copy %4\projects\asniffer_results\*.json %4\projects\%2
-rmdir %4\projects\asniffer_results\
-rmdir %4\asniffer\%2 
-del %4\asniffer\%3   
+cd ..\
+rmdir %4\asniffer\%2 /S /Q
+rmdir %4\projects\asniffer_results\ /S /Q
+   
